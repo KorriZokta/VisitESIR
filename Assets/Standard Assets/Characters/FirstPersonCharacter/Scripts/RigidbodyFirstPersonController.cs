571d7fd3d23713713a509e6a7e447bb5c5e7558f
KorriZokta <frederic.becker4@gmail.com>
2016-01-12 11:07:47 +0100

    Revert "Rescale"
    This reverts commit 9222a41b0db3166021c8a77c766457b723756a6e.

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -212,8 +212,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
             
             Vector2 input = new Vector2
                 {
-				x = Input.GetAxis("Horizontal"),
-				y = Input.GetAxis("Vertical")
+                    x = CrossPlatformInputManager.GetAxis("Horizontal"),
+                    y = CrossPlatformInputManager.GetAxis("Vertical")
                 };
 			movementSettings.UpdateDesiredTargetSpeed(input);
             return input;
